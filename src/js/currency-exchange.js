5195f1c0b887c36ebb7bea7cbc110cc53b151e74
Brianca Knight <briancaknight@gmail.com>
2023-11-17 10:15:25 -0800

    Add currrency-exchange file and styles.css file.

---
+++ src/js/currency-exchange.js
@@ -0,0 +1,19 @@
+export default class CurrencyExchange {
+  static getExchange(altCurrency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${altCurrency}`)
+      .then(function (response) {
+        if (!response.ok) {
+          return response.json()
+            .then(function (errorMessage) {
+              const errorText = `${response.status} ${response.statusText}`;
+              throw new Error(errorText);
+            });
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        return error;
+      });
+  }
+}
