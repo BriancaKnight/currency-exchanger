7130994a9b353d8ef71438b57e27d6da6cda7891
Brianca Knight <briancaknight@gmail.com>
2023-11-17 10:50:24 -0800

    Add printElements and printErrors functions.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ import CurrencyExchange from './js/currency-exchange';
 function getExchange(altCurrency) {
   CurrencyExchange.getExchange(altCurrency)
   .then (function (response) {
-    if (response.data) {
+    if (response.conversion_rates) {
       printElements(response, altCurrency);
     } else {
       printError(response, altCurrency);
     }
   });
 }
+
+function printElements(response, altCurrency) {
+  document.querySelector('#response-div').innerText = `The currency exchange for USD to ${altCurrency} is ${response.conversion_rates}`
+}
+
+function printError(eroror, altCurrency) {
+  document.querySelector('#response-div').innerText = `There was an error accessing the data for ${altCurrency}: ${error}`
+}
+
