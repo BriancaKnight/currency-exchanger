e8357e7961d7d2668243a7cc11b33a69d5a1849e
Brianca Knight <briancaknight@gmail.com>
2023-11-17 14:48:24 -0800

    finish conversion function and add clearResults function.

--- src/index.js
+++ src/index.js
@@ -6,33 +6,42 @@ import CurrencyExchange from './js/currency-exchange';
 function getExchange(altCurrency) {
   CurrencyExchange.getExchange(altCurrency)
   .then (function (response) {
-    if (response.conversion_rates) {
-      const conversion = convertCurrency(response);
-      printElements(response, altCurrency, conversion);
+    if (response.conversion_rates && response.conversion_rates[altCurrency]) {
+      const conversion = convertCurrency(response, altCurrency);
+      printElements(altCurrency, conversion);
     } else {
       printError(response, altCurrency);
     }
   });
 }
 
-function convertCurrency(response, altCurrency) {
-  let USD = parseInt(document.querySelector('#USD-amount').value);
-  let conversionRate = response.conversion_rates;
-  return USD / conversionRate;
+function handleForm (e) {
+  e.preventDefault();
+  const altCurrency = document.querySelector('#alt-currency').value;
+  getExchange(altCurrency);
+  clearResults();
 }
 
-function printElements(response, altCurrency, conversion) {
-  document.querySelector('#response-div').innerText = `The currency exchange for USD to ${altCurrency} is ${conversion}`;
+function convertCurrency(response, altCurrency) {
+  const conversionRate = response.conversion_rates[altCurrency];
+  const USD = parseInt(document.querySelector('#USD-amount').value);
+  return USD * conversionRate;
+}
+
+function printElements(altCurrency, conversion) {
+  const responseDiv = document.querySelector('#response-div');
+  const p = document.createElement('p');
+  p.append(`The currency exchange from USD to ${altCurrency} is ${conversion}`);
+  responseDiv.append(p);
 }
 
 function printError(error, altCurrency) {
   document.querySelector('#response-div').innerText = `There was an error accessing the data for ${altCurrency}: ${error}.`;
 }
 
-function handleForm (e) {
-  e.preventDefault();
-  const altCurrency = document.querySelector('#alt-currency').value;
-  getExchange(altCurrency);
+function clearResults() {
+  const responseDiv = document.getElementById('response-div');
+  responseDiv.innerText = ""
 }
 
 window.addEventListener("load", function () {
