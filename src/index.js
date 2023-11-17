a4f42b92e19255fe9937ee386d8e052727df863d
Brianca Knight <briancaknight@gmail.com>
2023-11-17 10:31:11 -0800

    Add getExchange function to index.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './js/currency-exchange';
+
+function getExchange(altCurrency) {
+  CurrencyExchange.getExchange(altCurrency)
+  .then (function (response) {
+    if (response.data) {
+      printElements(response, altCurrency);
+    } else {
+      printError(response, altCurrency);
+    }
+  });
+}
